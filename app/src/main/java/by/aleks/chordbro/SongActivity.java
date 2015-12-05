90a8e679a385f34b8008ef444df28f5403ba14e3
Alex Yeusiukou <xroker@gmail.com>
2015-12-05 18:31:03 +0100

    Styled SongActivity toolbar

--- app/src/main/java/by/aleks/chordbro/SongActivity.java
+++ app/src/main/java/by/aleks/chordbro/SongActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,7 @@ import android.text.Spanned;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -38,20 +40,15 @@ public class SongActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         final TextView songTV = (TextView)findViewById(R.id.song_text);
 
         final String artist = "Muse";
         final String title = "Uprising";
+        setTitle(artist + " - " + title);
+        ImageView artistImage = (ImageView)findViewById(R.id.artist_image);
+        artistImage.setImageResource(R.drawable.muse);
 
         new AsyncTask<Void, Void, Spanned>() {
             @Override
