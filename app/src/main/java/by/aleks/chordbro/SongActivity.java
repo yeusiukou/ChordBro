3b77b96ef658c16c66873986f12da12e5fa07671
Alex Yeusiukou <xroker@gmail.com>
2015-12-11 12:16:32 +0100

    Handled toolbar colapsing

--- app/src/main/java/by/aleks/chordbro/SongActivity.java
+++ app/src/main/java/by/aleks/chordbro/SongActivity.java
@@ -1,16 +1,21 @@
 package by.aleks.chordbro;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+
 import java.util.Map;
 import java.util.Set;
 
@@ -20,10 +25,10 @@ public class SongActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(
+        final CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(
                 R.id.toolbar_layout);
         collapsingToolbar.setTitleEnabled(false);
 
@@ -33,11 +38,23 @@ public class SongActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(artist + " - " + title);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
         ImageView artistImage = (ImageView)findViewById(R.id.artist_image);
         artistImage.setImageResource(R.drawable.muse);
 
         final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        // Set inner views behaviour on toolbar collapsing
+        AppBarLayout.OnOffsetChangedListener mListener = new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                float step = (collapsingToolbar.getHeight() - ViewCompat.getMinimumHeight(collapsingToolbar))/100;
+                float alpha = (collapsingToolbar.getHeight() - ViewCompat.getMinimumHeight(collapsingToolbar) + verticalOffset)/(step*100);
+                tabLayout.setAlpha(alpha); // fade tabs out on toolbar collapsing
+                toolbar.setTitleTextColor(adjustAlpha(0xFFFFFFFF, 1-alpha < 0 ? 0 : 1-alpha)); // fade in the title
+            }
+        };
+        AppBarLayout appBar = (AppBarLayout)findViewById(R.id.app_bar);
+        appBar.addOnOffsetChangedListener(mListener);
 
         // Load and display content
         new SongContentLoader(){
@@ -128,5 +145,13 @@ public class SongActivity extends AppCompatActivity {
             }
         }.execute(artist, title);
 
+    }
+
+    private int adjustAlpha(int color, float factor) {
+        int alpha = Math.round(Color.alpha(color) * factor);
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        return Color.argb(alpha, red, green, blue);
     }
 }
